d8fa659edff689a0504351183e855c32857d4775
Davi Izui <davi.izui@gmail.com>
2025-04-14 16:05:42 -0300

    first commit

---
+++ src/main/java/br/com/futurodev/jmt/exerciciosS08/ExerciciosS08Application.java
@@ -0,0 +1,13 @@
+package br.com.futurodev.jmt.exerciciosS08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosS08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosS08Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/futurodev/jmt/exerciciosS08/ExerciciosS08ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.futurodev.jmt.exerciciosS08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosS08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
